535d76a3b271e1f383f11c2252525392d7e78633
gotmachine <24925209+gotmachine@users.noreply.github.com>
2022-05-21 13:14:53 +0200

    Quick and dirty file reorganization. This thing is a mess.

---
+++ Source_PartScaler/KSPAddons/Tech.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace PartScaler
+{
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    public class TechUpdater : MonoBehaviour
+    {
+        public void Start()
+        {
+            Tech.Reload();
+        }
+    }
+
+    public static class Tech
+    {
+        private static HashSet<string> _unlockedTechs = new HashSet<string>();
+
+        public static void Reload()
+        {
+            if (HighLogic.CurrentGame == null)
+                return;
+            if (HighLogic.CurrentGame.Mode != Game.Modes.CAREER && HighLogic.CurrentGame.Mode != Game.Modes.SCIENCE_SANDBOX)
+                return;
+
+            var persistentfile = KSPUtil.ApplicationRootPath + "saves/" + HighLogic.SaveFolder + "/persistent.sfs";
+            var config = ConfigNode.Load(persistentfile);
+            var gameconf = config.GetNode("GAME");
+            var scenarios = gameconf.GetNodes("SCENARIO");
+            var thisScenario = scenarios.FirstOrDefault(a => a.GetValue("name") == "ResearchAndDevelopment");
+            if (thisScenario == null)
+                return;
+            var techs = thisScenario.GetNodes("Tech");
+
+            _unlockedTechs = techs.Select(a => a.GetValue("id")).ToHashSet();
+            _unlockedTechs.Add("");
+        }
+
+        public static bool IsUnlocked(string techId)
+        {
+            if (HighLogic.CurrentGame == null)
+                return true;
+            if (HighLogic.CurrentGame.Mode != Game.Modes.CAREER && HighLogic.CurrentGame.Mode != Game.Modes.SCIENCE_SANDBOX)
+                return true;
+            return techId == "" || _unlockedTechs.Contains(techId);
+        }
+    }
+}
